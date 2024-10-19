f1259e1d2477462bbe1fe785d0e3f13f7d302360
chexxrexx <98578633+chexxrexx@users.noreply.github.com>
2024-10-19 10:28:30 +0100

    basic commit

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from settings import *
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("duck duck go")
+        self.running = True
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.quit()
