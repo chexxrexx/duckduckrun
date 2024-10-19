108cb2cc0a49a6814642cc64db7d278640337488
chexxrexx <98578633+chexxrexx@users.noreply.github.com>
2024-10-19 10:55:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from settings import *
 
+from player import Player
+
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption("duck duck go")
+        pygame.display.set_caption("duck duck run")
+        self.clock = pygame.time.Clock()
         self.running = True
 
+        self.all_sprites = pygame.sprite.Group()
 
-pygame.quit()
+        self.player = Player((400,300), self.all_sprites)
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick()/1000
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.all_sprites.update(dt)
+
+            self.all_sprites.draw(self.display_surface)
+            pygame.display.update()
+
+        pygame.quit()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+
