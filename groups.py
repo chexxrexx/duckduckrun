81d997e482b26edb0a9889d69016c1a0f5be5af8
c-cynthia <cchen1293@gmail.com>
2024-10-19 12:59:40 +0100

    Update groups.py

--- groups.py
+++ groups.py
@@ -8,7 +8,7 @@ class AllSprites(pygame.sprite.Group):
         self.offset = pygame.Vector2()
 
 def draw(self, target_pos):
-    self.offset.x = -(target_pos [0] + WINDOW_WIDTH / 2)
+    self.offset.x = -(target_pos [0] - WINDOW_WIDTH / 2)
     self.offset.y = -(target_pos[1] - WINDOW_HEIGHT / 2)
 
     ground_sprites = [sprites for sprite in self if hassattr(sprite, 'ground')]
