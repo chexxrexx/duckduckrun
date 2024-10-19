a56555a7abc971b6027a78b2cb0282cefc85d446
c-cynthia <cchen1293@gmail.com>
2024-10-19 11:54:54 +0100

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -6,3 +6,4 @@ class CollisonSprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image.fill('blue') # Fills the image with a blue colour
         self.rect = self.image.get_rect(center = pos) # Creates the rectangle and positions it at center pos
+        self.ground = True #This allows the ground to be layered correctly beneath other elements
